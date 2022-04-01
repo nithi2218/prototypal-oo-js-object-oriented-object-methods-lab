555985a4519b3441016afba68f632486eb24ab0f
Nithyadevi <nithi2218@gmail.com>
2022-04-01 23:50:08 +1100

    Assingment updated

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-class BoardMember {
-    constructor(name, homeState, training) {
+function BoardMember(name, homeState, training) {
         this.name = name;
         this.homeState = homeState;
         this.training = training;
     }
-    veto() {
+    BoardMember.prototype.veto = function() {
         return "No, I must disagree";
     }
-    approve() {
+    BoardMember.prototype.approve = function() {
         return "You can do that!";
     }
-    doCharity() {
+    BoardMember.prototype.doCharity = function() {
         return "I like to help people.";
     }
-    releasePressStatement() {
+    BoardMember.prototype.releasePressStatement = function()  {
         return "You will see great things from Scuber.";
     }
-    sayHi() {
+    BoardMember.prototype.sayHi = function()  {
         return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     }
-}
